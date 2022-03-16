6374327df65b5f9a67e199820cf19c9c8330ffa0
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-16 14:53:42 +0200

    Exception handler

---
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/exceptions/GlobalExceptionHandlerController.java
@@ -0,0 +1,18 @@
+package com.odeyalo.netflix.emailsenderservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.mail.MessagingException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandlerController {
+
+    @ExceptionHandler(value = MessagingException.class)
+    public ResponseEntity<?> handleMessagingException(MessagingException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
