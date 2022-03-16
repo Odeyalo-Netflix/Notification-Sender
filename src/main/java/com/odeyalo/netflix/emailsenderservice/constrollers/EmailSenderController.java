5767ced84ec14e1e66df919864c5fe90934029e0
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-16 14:51:36 +0200

    Controller that  sends messages

--- src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
@@ -1,7 +1,28 @@
 package com.odeyalo.netflix.emailsenderservice.constrollers;
 
+import com.odeyalo.netflix.emailsenderservice.dto.EmailMessageDTO;
+import com.odeyalo.netflix.emailsenderservice.service.EmailSenderManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
+
 @RestController
+@RequestMapping("/api/v1")
 public class EmailSenderController {
+    private final EmailSenderManager senderManager;
+
+    public EmailSenderController(EmailSenderManager senderManager) {
+        this.senderManager = senderManager;
+    }
+
+    @GetMapping("/send")
+    public ResponseEntity<?> sendMail(@RequestBody EmailMessageDTO dto) throws MessagingException {
+        this.senderManager.send(dto.getBody(), dto.getSubject(), dto.getTo());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
