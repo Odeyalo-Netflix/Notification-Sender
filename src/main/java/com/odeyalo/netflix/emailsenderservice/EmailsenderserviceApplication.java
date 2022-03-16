c8d2186b296a5bc6e12267c0e97f2f362eda50c6
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-16 14:52:28 +0200

    Added async support and enabled eureka client

--- src/main/java/com/odeyalo/netflix/emailsenderservice/EmailsenderserviceApplication.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/EmailsenderserviceApplication.java
@@ -2,8 +2,12 @@ package com.odeyalo.netflix.emailsenderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableEurekaClient
 public class EmailsenderserviceApplication {
 
     public static void main(String[] args) {
