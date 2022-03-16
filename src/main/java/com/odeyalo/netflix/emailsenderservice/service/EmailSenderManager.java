26f5a1d9b2c2dd4b35f522c9ee3cd2723d41ecbe
Odeyalo <theprogrammerinfuture@gmail.com>
2022-03-16 14:51:54 +0200

    Interface for all email senders

--- src/main/java/com/odeyalo/netflix/emailsenderservice/service/EmailSenderManager.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/EmailSenderManager.java
@@ -1,7 +1,9 @@
 package com.odeyalo.netflix.emailsenderservice.service;
 
+import javax.mail.MessagingException;
+
 public interface EmailSenderManager {
 
-    void send(String body, String subject, String to);
+    void send(String body, String subject, String to) throws MessagingException;
 
 }
