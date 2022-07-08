266d411867d4728afdb64b318ca1f136ce19bec4
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-08 19:56:53 +0300

    Listen to topic and send SMS message to phone number

---
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/kafka/listener/PhoneNumberSmsDtoKafkaMessageListener.java
@@ -0,0 +1,27 @@
+package com.odeyalo.netflix.emailsenderservice.service.kafka.listener;
+
+import com.odeyalo.netflix.emailsenderservice.dto.PhoneNumberSmsDTO;
+import com.odeyalo.netflix.emailsenderservice.service.sender.sms.PhoneNumberMessageSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhoneNumberSmsDtoKafkaMessageListener implements KafkaMessageListener<PhoneNumberSmsDTO>{
+    private final PhoneNumberMessageSender phoneNumberMessageSender;
+
+    private final Logger logger = LoggerFactory.getLogger(PhoneNumberSmsDtoKafkaMessageListener.class);
+    public PhoneNumberSmsDtoKafkaMessageListener(PhoneNumberMessageSender phoneNumberMessageSender) {
+        this.phoneNumberMessageSender = phoneNumberMessageSender;
+    }
+
+    @Override
+    @KafkaListener(topics = "PHONE_NUMBER_SMS_SENDER", containerFactory = "phoneNumberSmsDTOConcurrentKafkaListenerContainerFactory")
+    public void listen(PhoneNumberSmsDTO phoneNumberSmsDTO) throws Exception {
+        this.logger.info("Accept message: {}", phoneNumberSmsDTO);
+        this.phoneNumberMessageSender.sendMessage(
+                phoneNumberSmsDTO.getPhoneNumber(), phoneNumberSmsDTO.getBody()
+        );
+    }
+}
