905c732e24cf5eb6703625836ffaad6535eede66
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-08 20:06:15 +0300

    Listens topic and sends email messages to specific email

---
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/kafka/listener/EmailMessageDTOKafkaMessageListener.java
@@ -0,0 +1,28 @@
+package com.odeyalo.netflix.emailsenderservice.service.kafka.listener;
+
+import com.odeyalo.netflix.emailsenderservice.dto.EmailMessageDTO;
+import com.odeyalo.netflix.emailsenderservice.service.sender.email.EmailSenderManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+
+@Service
+public class EmailMessageDTOKafkaMessageListener implements KafkaMessageListener<EmailMessageDTO> {
+    private final Logger logger = LoggerFactory.getLogger(EmailMessageDTOKafkaMessageListener.class);
+    private final EmailSenderManager manager;
+
+    public EmailMessageDTOKafkaMessageListener(@Qualifier("simpleAsyncEmailSenderManager") EmailSenderManager manager) {
+        this.manager = manager;
+    }
+
+    @KafkaListener(topics = {"MAIL_MESSAGE_SENDER_TOPIC"},
+            containerFactory = "emailMessageDTOConcurrentKafkaListenerContainerFactory")
+    public void listen(EmailMessageDTO dto) throws MessagingException {
+        this.logger.info("New message: to: {}, body: {}", dto.getTo(), dto.getBody());
+        this.manager.send(dto.getBody(), dto.getSubject(), dto.getTo());
+    }
+}
