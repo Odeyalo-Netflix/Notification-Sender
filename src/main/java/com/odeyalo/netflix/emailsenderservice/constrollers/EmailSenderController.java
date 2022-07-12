5bb3afc7580750dcefd62cf85d1f596e192990df
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-12 19:07:09 +0300

    Added Qualifier and EmailSender

--- src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
@@ -1,7 +1,8 @@
 package com.odeyalo.netflix.emailsenderservice.constrollers;
 
 import com.odeyalo.netflix.emailsenderservice.dto.EmailMessageDTO;
-import com.odeyalo.netflix.emailsenderservice.service.sender.email.GoogleXOauth2AsyncMailSender;
+import com.odeyalo.netflix.emailsenderservice.service.sender.email.EmailSender;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,9 @@ import java.security.GeneralSecurityException;
 @RestController
 @RequestMapping("/api/v1")
 public class EmailSenderController {
-    private final GoogleXOauth2AsyncMailSender sender;
+    private final EmailSender sender;
 
-    public EmailSenderController(GoogleXOauth2AsyncMailSender sender) {
+    public EmailSenderController(@Qualifier("googleXOauth2AsyncMailSender") EmailSender sender) {
         this.sender = sender;
     }
 
