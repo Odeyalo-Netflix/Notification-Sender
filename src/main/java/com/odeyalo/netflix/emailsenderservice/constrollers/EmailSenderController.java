65b24cbb99c964facda9d412b2831b1ea9e0dc1c
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-12 14:30:06 +0300

    Updated controller

--- src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
@@ -1,30 +1,27 @@
 package com.odeyalo.netflix.emailsenderservice.constrollers;
 
 import com.odeyalo.netflix.emailsenderservice.dto.EmailMessageDTO;
-import com.odeyalo.netflix.emailsenderservice.service.sender.email.EmailSenderManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.scheduling.annotation.Async;
+import com.odeyalo.netflix.emailsenderservice.service.sender.email.GoogleXOauth2AsyncMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmailSenderController {
-    private final EmailSenderManager senderManager;
+    private final GoogleXOauth2AsyncMailSender sender;
 
-    @Autowired
-    public EmailSenderController(@Qualifier("simpleAsyncEmailSenderManager") EmailSenderManager senderManager) {
-        this.senderManager = senderManager;
+    public EmailSenderController(GoogleXOauth2AsyncMailSender sender) {
+        this.sender = sender;
     }
 
-    @Async
     @PostMapping("/send")
-    public void sendMail(@RequestBody EmailMessageDTO dto) throws MessagingException {
-        this.senderManager.send(dto.getBody(), dto.getSubject(), dto.getTo());
+    public void sendMail(@RequestBody EmailMessageDTO dto) throws MessagingException, IOException, GeneralSecurityException {
+        this.sender.send(dto.getBody(), dto.getSubject(), dto.getTo());
     }
 }
