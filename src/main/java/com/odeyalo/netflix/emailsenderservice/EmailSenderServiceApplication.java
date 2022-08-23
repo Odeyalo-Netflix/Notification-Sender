24c42be18368a544ec85171b7c2756acac44d85f
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-23 13:00:02 +0300

    Added scheduling

--- src/main/java/com/odeyalo/netflix/emailsenderservice/EmailSenderServiceApplication.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/EmailSenderServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync(proxyTargetClass = true)
 @EnableEurekaClient
 @EnableKafka
+@EnableScheduling
 public class EmailSenderServiceApplication {
 
     public static void main(String[] args) {
