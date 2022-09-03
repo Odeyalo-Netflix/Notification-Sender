97cee6d8cb8581f78b1cc99f77b99a55474bb771
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-03 12:39:58 +0300

    MimeMessage now used instead EmailMessageDTO

--- src/main/java/com/odeyalo/netflix/emailsenderservice/service/sender/email/CachingEmailSender.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/sender/email/CachingEmailSender.java
@@ -1,16 +1,15 @@
 package com.odeyalo.netflix.emailsenderservice.service.sender.email;
 
 
-import com.odeyalo.support.clients.notification.dto.EmailMessageDTO;
-
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 public interface CachingEmailSender extends EmailSender {
     /**
-     * Cache specific message that will be sended in future
+     * Cache specific message that will be sent in future
      * @param message - message to cache
      */
-    void cacheMessage(EmailMessageDTO message);
+    void cacheMessage(MimeMessage message);
 
     /**
      * Sends cached messages
