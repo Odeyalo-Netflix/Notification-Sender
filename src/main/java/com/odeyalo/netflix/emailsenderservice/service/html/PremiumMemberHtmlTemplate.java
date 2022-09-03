2f5fc4a2dcf9c55f4829903803ec8340725ccacf
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-03 12:46:22 +0300

    HtmlTemplate that returns a template with email letter

---
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/html/PremiumMemberHtmlTemplate.java
@@ -0,0 +1,30 @@
+package com.odeyalo.netflix.emailsenderservice.service.html;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Component
+public class PremiumMemberHtmlTemplate implements HtmlTemplate {
+    public static final String TEMPLATE_TYPE_NAME = "PREMIUM_MEMBER_EMAIL_LETTER";
+    private final String path = "C:\\Users\\thepr_2iz2cnv\\IdeaProjects\\emailsenderservice\\src\\main\\resources\\templates\\premium.html"; //todo
+    private String cache;
+
+    @Override
+    public String getHtmlTemplate() throws IOException {
+        if (cache != null) {
+            return cache;
+        }
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        String body = new String(bytes);
+        this.cache = body;
+        return body;
+    }
+
+    @Override
+    public String getType() {
+        return TEMPLATE_TYPE_NAME;
+    }
+}
