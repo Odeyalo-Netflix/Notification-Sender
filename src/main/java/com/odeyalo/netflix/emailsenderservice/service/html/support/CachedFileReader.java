aa1f0645d6e57d1670ac1cebb255cbc262a4b03f
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-12 17:44:32 +0300

    Reader that cache read data

---
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/html/support/CachedFileReader.java
@@ -0,0 +1,14 @@
+package com.odeyalo.netflix.emailsenderservice.service.html.support;
+
+import java.nio.file.Path;
+
+/**
+ * Represents a file reader with cached function
+ */
+public interface CachedFileReader {
+
+    byte[] readAllBytes(Path path);
+
+    boolean isAlreadyCached(Path path);
+
+}
