35e3bc38afa11a26c9a00f6667e252dee3a3e22d
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-12 17:34:44 +0300

    Simple file reader that cache the data that was read

---
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/html/support/SimpleCachedFileReader.java
@@ -0,0 +1,36 @@
+package com.odeyalo.netflix.emailsenderservice.service.html.support;
+
+import com.odeyalo.netflix.emailsenderservice.exceptions.FileReadingFailedException;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SimpleCachedFileReader implements CachedFileReader {
+    /**
+     * Map that contains cache
+     * Key - path to file
+     * Value - file bytes
+     */
+    private final Map<String, byte[]> cache = new HashMap<>();
+
+    @Override
+    public byte[] readAllBytes(Path path) {
+        try {
+            byte[] bytes = Files.readAllBytes(path);
+            this.cache.put(path.toAbsolutePath().toString(), bytes);
+            return bytes;
+        } catch (IOException e) {
+            throw new FileReadingFailedException("File reading reading was failed", "We cannot process it right now. Please, try again later", e);
+        }
+    }
+
+    @Override
+    public boolean isAlreadyCached(Path path) {
+        return cache.containsKey(path.toAbsolutePath().toString());
+    }
+}
