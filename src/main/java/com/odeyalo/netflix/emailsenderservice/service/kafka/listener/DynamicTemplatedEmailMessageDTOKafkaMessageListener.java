f66f2264d3ee91244e8568712c2508bb7c67b682
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-13 10:44:58 +0300

    Changed imports

--- src/main/java/com/odeyalo/netflix/emailsenderservice/service/kafka/listener/DynamicTemplatedEmailMessageDTOKafkaMessageListener.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/service/kafka/listener/DynamicTemplatedEmailMessageDTOKafkaMessageListener.java
@@ -1,7 +1,7 @@
 package com.odeyalo.netflix.emailsenderservice.service.kafka.listener;
 
-import com.odeyalo.netflix.emailsenderservice.service.html.AdvancedHtmlTemplateFactory;
-import com.odeyalo.netflix.emailsenderservice.service.html.DynamicHtmlTemplate;
+import com.odeyalo.netflix.emailsenderservice.service.html.AdvancedHtmlTemplateProviderFactory;
+import com.odeyalo.netflix.emailsenderservice.service.html.DynamicHtmlTemplateProvider;
 import com.odeyalo.netflix.emailsenderservice.service.support.IncomingEmailMessageCachingService;
 import com.odeyalo.netflix.emailsenderservice.service.support.MimeMessageBuilder;
 import com.odeyalo.support.clients.notification.dto.DynamicTemplatedEmailMessageDTO;
@@ -17,11 +17,11 @@ import java.util.Map;
 @Component
 public class DynamicTemplatedEmailMessageDTOKafkaMessageListener implements KafkaMessageListener<DynamicTemplatedEmailMessageDTO> {
     private final IncomingEmailMessageCachingService cachingService;
-    private final AdvancedHtmlTemplateFactory factory;
+    private final AdvancedHtmlTemplateProviderFactory factory;
     private final MimeMessageBuilder builder;
 
     @Autowired
-    public DynamicTemplatedEmailMessageDTOKafkaMessageListener(IncomingEmailMessageCachingService cachingService, AdvancedHtmlTemplateFactory factory, MimeMessageBuilder builder) {
+    public DynamicTemplatedEmailMessageDTOKafkaMessageListener(IncomingEmailMessageCachingService cachingService, AdvancedHtmlTemplateProviderFactory factory, MimeMessageBuilder builder) {
         this.cachingService = cachingService;
         this.factory = factory;
         this.builder = builder;
@@ -32,7 +32,7 @@ public class DynamicTemplatedEmailMessageDTOKafkaMessageListener implements Kafk
     public void listen(DynamicTemplatedEmailMessageDTO dto) throws Exception {
         Map<String, Object> modelMap = dto.getModel();
         String templateType = dto.getTemplateType();
-        DynamicHtmlTemplate template = factory.getDynamicHtmlTemplate(templateType);
+        DynamicHtmlTemplateProvider template = factory.getDynamicHtmlTemplate(templateType);
         ExtendedModelMap model = new ExtendedModelMap();
         model.addAllAttributes(modelMap);
         String body = template.getHtmlTemplate(model);
