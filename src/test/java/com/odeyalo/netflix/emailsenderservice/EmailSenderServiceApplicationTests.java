1164bf5b56b706e3d0dd9bd2ab82fb554270cb96
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-13 10:45:41 +0300

    Added TestPropertySource annotation

--- src/test/java/com/odeyalo/netflix/emailsenderservice/EmailSenderServiceApplicationTests.java
+++ src/test/java/com/odeyalo/netflix/emailsenderservice/EmailSenderServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.odeyalo.netflix.emailsenderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(value = "classpath:application.properties")
 class EmailSenderServiceApplicationTests {
 
     @Test
