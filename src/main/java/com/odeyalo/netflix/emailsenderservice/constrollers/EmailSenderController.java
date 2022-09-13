74045030c424be09922abfe83a9f99b26f38d80f
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-13 10:45:11 +0300

    Changed imports

--- src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/constrollers/EmailSenderController.java
@@ -1,13 +1,9 @@
 package com.odeyalo.netflix.emailsenderservice.constrollers;
 
-import com.odeyalo.netflix.emailsenderservice.service.html.CodeVerificationEmailLetterDynamicHtmlTemplate;
-import com.odeyalo.netflix.emailsenderservice.service.html.HtmlTemplateFactory;
 import com.odeyalo.netflix.emailsenderservice.service.sender.email.CachingEmailSender;
 import com.odeyalo.netflix.emailsenderservice.service.support.MimeMessageBuilder;
-import com.odeyalo.support.clients.notification.dto.DynamicTemplatedEmailMessageDTO;
 import com.odeyalo.support.clients.notification.dto.EmailMessageDTO;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +13,6 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/v1")
