8e3acff63d5f0eb8dd910bd63e954046402935ac
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-18 21:38:28 +0300

    Entity that contains info about the sent email

---
+++ src/main/java/com/odeyalo/netflix/emailsenderservice/entity/SentEmail.java
@@ -0,0 +1,29 @@
+package com.odeyalo.netflix.emailsenderservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SentEmail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "recipient", nullable = false, updatable = false, length = 100)
+    private String to;
+    @Column(nullable = false, updatable = false, length = 5000)
+    private String body;
+    @Column(nullable = false, updatable = false)
+    private String subject;
+    @Column(nullable = false, updatable = false)
+    private boolean success;
+    @Column(updatable = false)
+    private String exceptionReason;
+}
